d71b04f363819b6c90bce71b3ae06ae2174afaa9
Qiong Yu <sb15897@student.samk.fi>
2025-11-01 16:51:19 +0000

    add EfficientNet model

--- args.py
+++ args.py
@@ -3,19 +3,22 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(description='Model training options')
 
-    parser.add_argument('--backbone', type=str, default='resnet50',
-                        choices=['resnet18', 'resnet34', 'resnet50'])
+    parser.add_argument('--backbone', type=str, default='efficientnet_b0',
+                        choices=['resnet18', 'resnet34', 'resnet50',
+                                'efficientnet_b0', 'efficientnet_b1', 'efficientnet_b2',
+                                'efficientnet_b3', 'efficientnet_b4', 'efficientnet_b5',
+                                'efficientnet_b6', 'efficientnet_b7'])
 
     parser.add_argument('--csv_dir', type=str, default='CSVs')
-    parser.add_argument('--batch_size', type=int, default=48,
+    parser.add_argument('--batch_size', type=int, default=32,
                         choices=[16, 32, 48, 64])
 
-    parser.add_argument('--lr', type=float, default=5e-5)
+    parser.add_argument('--lr', type=float, default=1e-4)
     
-    parser.add_argument('--weight_decay', type=float, default=2e-4, 
+    parser.add_argument('--weight_decay', type=float, default=1e-4, 
                         help='L2 regularization strength to prevent overfitting')
 
-    parser.add_argument('--epochs', type=int, default=35)
+    parser.add_argument('--epochs', type=int, default=50)
 
     parser.add_argument('--out_dir', type=str, default='breast_session')
 
@@ -23,3 +26,31 @@ def get_args():
 
     args = parser.parse_args()
     return args
+
+
+
+# import argparse
+
+# def get_args():
+#     parser = argparse.ArgumentParser(description='Model training options')
+
+#     parser.add_argument('--backbone', type=str, default='resnet50',
+#                         choices=['resnet18', 'resnet34', 'resnet50'])
+
+#     parser.add_argument('--csv_dir', type=str, default='CSVs')
+#     parser.add_argument('--batch_size', type=int, default=48,
+#                         choices=[16, 32, 48, 64])
+
+#     parser.add_argument('--lr', type=float, default=5e-5)
+    
+#     parser.add_argument('--weight_decay', type=float, default=2e-4, 
+#                         help='L2 regularization strength to prevent overfitting')
+
+#     parser.add_argument('--epochs', type=int, default=35)
+
+#     parser.add_argument('--out_dir', type=str, default='breast_session')
+
+#     parser.add_argument('--seed', type=int, default=42, help='random seed for reproducibility')
+
+#     args = parser.parse_args()
+#     return args
--- model.py
+++ model.py
@@ -1,26 +1,44 @@
-
 import torch
 import torch.nn as nn
 import torchvision.models as models
 
 class BreastModel(nn.Module):
-    def __init__(self, backbone='resnet18', num_classes=3):
+    def __init__(self, backbone='efficientnet_b0', num_classes=3):
         super(BreastModel, self).__init__()
 
-        # use new weights API
-        if backbone == 'resnet18':
-            weights = models.ResNet18_Weights.IMAGENET1K_V1
-            self.model = models.resnet18(weights=weights)
-        elif backbone == 'resnet34':
-            weights = models.ResNet34_Weights.IMAGENET1K_V1
-            self.model = models.resnet34(weights=weights)
+        # use new weights API for EfficientNet
+        if backbone == 'efficientnet_b0':
+            weights = models.EfficientNet_B0_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b0(weights=weights)
+        elif backbone == 'efficientnet_b1':
+            weights = models.EfficientNet_B1_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b1(weights=weights)
+        elif backbone == 'efficientnet_b2':
+            weights = models.EfficientNet_B2_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b2(weights=weights)
+        elif backbone == 'efficientnet_b3':
+            weights = models.EfficientNet_B3_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b3(weights=weights)
+        elif backbone == 'efficientnet_b4':
+            weights = models.EfficientNet_B4_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b4(weights=weights)
+        elif backbone == 'efficientnet_b5':
+            weights = models.EfficientNet_B5_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b5(weights=weights)
+        elif backbone == 'efficientnet_b6':
+            weights = models.EfficientNet_B6_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b6(weights=weights)
+        elif backbone == 'efficientnet_b7':
+            weights = models.EfficientNet_B7_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b7(weights=weights)
         else:
-            weights = models.ResNet50_Weights.IMAGENET1K_V1
-            self.model = models.resnet50(weights=weights)
+            # 默认使用efficientnet_b0
+            weights = models.EfficientNet_B0_Weights.IMAGENET1K_V1
+            self.model = models.efficientnet_b0(weights=weights)
 
         # modify first conv layer to accept single-channel input
-        original_conv = self.model.conv1
-        self.model.conv1 = nn.Conv2d(
+        original_conv = self.model.features[0][0]  # EfficientNet的第一个卷积层
+        self.model.features[0][0] = nn.Conv2d(
             in_channels=1,  # single-channel input
             out_channels=original_conv.out_channels,
             kernel_size=original_conv.kernel_size,
@@ -30,7 +48,54 @@ class BreastModel(nn.Module):
         )
 
         # modify last fully connected layer for 3-class classification
-        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+        # EfficientNet使用classifier而不是fc
+        original_classifier = self.model.classifier[1]
+        self.model.classifier[1] = nn.Linear(
+            original_classifier.in_features, 
+            num_classes
+        )
 
     def forward(self, x):
         return self.model(x)
+
+
+
+
+
+
+
+# import torch
+# import torch.nn as nn
+# import torchvision.models as models
+
+# class BreastModel(nn.Module):
+#     def __init__(self, backbone='resnet18', num_classes=3):
+#         super(BreastModel, self).__init__()
+
+#         # use new weights API
+#         if backbone == 'resnet18':
+#             weights = models.ResNet18_Weights.IMAGENET1K_V1
+#             self.model = models.resnet18(weights=weights)
+#         elif backbone == 'resnet34':
+#             weights = models.ResNet34_Weights.IMAGENET1K_V1
+#             self.model = models.resnet34(weights=weights)
+#         else:
+#             weights = models.ResNet50_Weights.IMAGENET1K_V1
+#             self.model = models.resnet50(weights=weights)
+
+#         # modify first conv layer to accept single-channel input
+#         original_conv = self.model.conv1
+#         self.model.conv1 = nn.Conv2d(
+#             in_channels=1,  # single-channel input
+#             out_channels=original_conv.out_channels,
+#             kernel_size=original_conv.kernel_size,
+#             stride=original_conv.stride,
+#             padding=original_conv.padding,
+#             bias=original_conv.bias is not None
+#         )
+
+#         # modify last fully connected layer for 3-class classification
+#         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+
+#     def forward(self, x):
+#         return self.model(x)
